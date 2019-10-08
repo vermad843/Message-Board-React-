4697864e2a795b2c632fa9371178268dfa574f2e
vermad843 <vishnuverma7524@gmail.com>
2019-10-08 20:33:40 +0530

    Login page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor() {
     super();
    this.state = {
-     title : 'Signup ' ,
+     title : 'Login ' ,
     }
   }
   render() {
@@ -14,14 +14,12 @@ class App extends Component {
     return (
       <div className = "App">
       <h1 className= "title">{title}</h1>
-       <form  className ="signup-form">
+       <form  className ="login-form">
       <h6> <label  htmlFor = "username">Username</label></h6>
        <input className = "u-full-width" type ="text" id = "username" name = "username" />  
        <h6> <label  htmlFor = "password">Password</label></h6>
        <input className = "u-full-width" type ="text" id = "password" name = "password" /> 
-       <h6> <label  htmlFor = "confirmpassword">confirmPassword</label></h6>
-       <input className = "u-full-width" type ="text" id = "confirmpassword" name = "confirmpassword" /> 
-       <button type = "submit" className ="button-primary">Sign Up</button>
+     <h6> <button type = "submit" className ="button-primary">Login</button></h6> 
      </form>
    </div>
     );
