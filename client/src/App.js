cfb58bff51297c6470be97efa126c098a9630cae
vermad843 <vishnuverma7524@gmail.com>
2019-10-10 11:43:50 +0530

    hiding the form when user presssubmit button

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 
+
 class App extends Component {
   constructor() {
     super();
@@ -9,12 +10,14 @@ class App extends Component {
       message : 'Message Board',
       name : '',
       mew : '', 
+      done : null ,
       mews : [{
         name : "Dk",
         mew  : "wow",
       }],
-      loading : false 
-    }
+      loading : false ,
+      done : false,
+    };
   }
 
 
@@ -36,7 +39,8 @@ class App extends Component {
     event.preventDefault();
 
     this.setState({
-     loading : true
+     loading : true,
+     done : true
     });
    const mews = this.state.mews;
    console.log(mews);
@@ -55,20 +59,19 @@ class App extends Component {
 
 
 
-
-
   render() {
     const { message,loading}  = this.state;
     return (
       <div>
       <h1 className = "title">{message}</h1>
-        <form onSubmit ={this.formSubmitted.bind(this)} className ="mew-form">
+      { !this.state.done && ( 
+        <form onSubmit ={this.formSubmitted.bind(this)} className ="mew-form"  >
           <label  htmlFor = "name">Name</label>
           <input onChange = {this.nameChanged.bind(this)} className = "u-full-width" type ="text" id = "name" name = "name" />  
           <label  htmlFor = "content">Mew</label>
           <textarea  onChange = {this.mewChanged.bind(this)} className = "u-full-width" type ="text" id = "mew" name = "mew"></textarea> 
           <button type = "submit" className ="button-primary">Submit</button>
-      </form>
+      </form> )}
       <div className = "loading">
       {loading ? <img  src = "Facebook-1s-200px.gif" alt = "loading" /> : '' }
       </div>
